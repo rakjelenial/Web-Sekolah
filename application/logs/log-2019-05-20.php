ecf693a1bd6ffb1a9d0dc61228a8680ade95a147
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-05-20 09:16:07 +0800

    First Upload

---
+++ application/logs/log-2019-05-20.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-20 01:05:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-20 01:05:38 --> Unable to connect to the database
+ERROR - 2019-05-20 01:06:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-20 01:06:45 --> Unable to connect to the database
+ERROR - 2019-05-20 01:07:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'hendri2212'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-20 01:07:00 --> Unable to connect to the database
+ERROR - 2019-05-20 01:07:06 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'db_cms_sekolahku' D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-20 01:07:06 --> Unable to connect to the database
+ERROR - 2019-05-20 01:07:17 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-20 01:15:14 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-20 02:04:32 --> 404 Page Not Found: Assets/plugins
+ERROR - 2019-05-20 02:18:11 --> 404 Page Not Found: Assets/plugins
