cb7e5c518ff448acb80953d76ff09241e223bd7a
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-05-27 08:11:54 +0800

    ssl, copyright, view code

---
+++ application/logs/log-2019-05-26.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-26 13:07:12 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-26 13:28:01 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-26 13:52:31 --> 404 Page Not Found: Media_library/images
---
+++ application/logs/log-2019-05-27.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-27 01:53:50 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 01:54:09 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 01:59:59 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:01:03 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:03:40 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:03:57 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:05:11 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:06:08 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:07:17 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:09:28 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:10:24 --> 404 Page Not Found: Media_library/images
