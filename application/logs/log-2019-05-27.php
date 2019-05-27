d09013275d57445ab357fdb6232cedef8a7786a4
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-05-27 08:48:45 +0800

    logo sekolah

--- application/logs/log-2019-05-27.php
+++ application/logs/log-2019-05-27.php
@@ -11,3 +11,19 @@ ERROR - 2019-05-27 02:06:08 --> 404 Page Not Found: Media_library/images
 ERROR - 2019-05-27 02:07:17 --> 404 Page Not Found: Media_library/images
 ERROR - 2019-05-27 02:09:28 --> 404 Page Not Found: Media_library/images
 ERROR - 2019-05-27 02:10:24 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:44:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smknkot2_websmk'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-27 02:44:30 --> Unable to connect to the database
+ERROR - 2019-05-27 02:44:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smknkot2_websmk'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-27 02:44:30 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time D:\xampp\htdocs\websmk\system\libraries\Session\Session_driver.php 205
+ERROR - 2019-05-27 02:44:30 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: D:\xampp\tmp) D:\xampp\htdocs\websmk\system\libraries\Session\Session.php 143
+ERROR - 2019-05-27 02:44:30 --> Severity: error --> Exception: Call to a member function real_escape_string() on boolean D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2019-05-27 02:44:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smknkot2_websmk'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-27 02:44:40 --> Unable to connect to the database
+ERROR - 2019-05-27 02:44:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smknkot2_websmk'@'localhost' (using password: YES) D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2019-05-27 02:44:40 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time D:\xampp\htdocs\websmk\system\libraries\Session\Session_driver.php 205
+ERROR - 2019-05-27 02:44:40 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: D:\xampp\tmp) D:\xampp\htdocs\websmk\system\libraries\Session\Session.php 143
+ERROR - 2019-05-27 07:44:40 --> Severity: error --> Exception: Call to a member function real_escape_string() on boolean D:\xampp\htdocs\websmk\system\database\drivers\mysqli\mysqli_driver.php 393
+ERROR - 2019-05-27 02:46:38 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:46:43 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:46:58 --> 404 Page Not Found: Media_library/images
+ERROR - 2019-05-27 02:47:07 --> 404 Page Not Found: Media_library/images
