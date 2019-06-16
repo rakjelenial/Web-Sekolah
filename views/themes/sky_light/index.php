e89451f26f987962e718e2b9bced1ed35cb97ab6
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-06-16 15:15:55 +0800

    htaccess new

--- views/themes/sky_light/index.php
+++ views/themes/sky_light/index.php
@@ -4,7 +4,7 @@
 	<title><?=isset($page_title) ? $page_title . ' | ' : ''?><?=__session('school_name')?></title>
 
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
          (adsbygoogle = window.adsbygoogle || []).push({
               google_ad_client: "ca-pub-2586125774271995",
